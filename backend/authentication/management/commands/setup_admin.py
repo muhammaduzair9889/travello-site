2150c003852219d779b062ec8db5c145f0d58ecd
Muhammad Uzair <muhammaduzair9889@gmail.com>
2026-03-09 01:14:55 +0500

    security: replace hardcoded admin password with env var in setup_admin command

--- backend/authentication/management/commands/setup_admin.py
+++ backend/authentication/management/commands/setup_admin.py
@@ -1,4 +1,6 @@
-from django.core.management.base import BaseCommand
+import os
+
+from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -7,10 +9,20 @@ User = get_user_model()
 class Command(BaseCommand):
     help = 'Creates a default admin user if it does not exist'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--email', default=os.getenv('ADMIN_EMAIL', 'superadmin@travello.com'))
+        parser.add_argument('--username', default=os.getenv('ADMIN_USERNAME', 'superadmin'))
+        parser.add_argument('--password', default=os.getenv('ADMIN_PASSWORD'))
+
     def handle(self, *args, **options):
-        email = 'superadmin@travello.com'
-        password = '***REMOVED***'
-        username = 'superadmin'
+        email = options['email']
+        username = options['username']
+        password = options['password']
+
+        if not password:
+            raise CommandError(
+                'Admin password must be provided via --password flag or ADMIN_PASSWORD env var.'
+            )
 
         if User.objects.filter(email=email).exists():
             self.stdout.write(
